598e10252853d07b47db5b02a3430b7345a7a10a
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-07-07 23:17:31 -0700

    Add emacs daemon mode cursor fix.

--- confs/home/geoff/.doom.d/config.el
+++ confs/home/geoff/.doom.d/config.el
@@ -123,3 +123,12 @@
           (lambda ()
             (setq lsp-python-ms-extra-paths
                   (vconcat lsp-python-ms-extra-paths (vector (get-dir))))))
+
+;; When running Emacs in daemon mode, cursor colour is altered by Xresources
+;; This changes it back.
+(require 'frame)
+(defun set-cursor-hook (frame)
+(modify-frame-parameters
+  frame (list (cons 'cursor-color "#fb2874"))))  ;; molokai magenta
+
+(add-hook 'after-make-frame-functions 'set-cursor-hook)
