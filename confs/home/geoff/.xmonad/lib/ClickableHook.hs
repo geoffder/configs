8215c411cf5a598616d2f1e8d2e1ee0ae9945f0b
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-07-12 21:07:20 -0700

    Comments and clean-up,

--- confs/home/geoff/.xmonad/lib/ClickableHook.hs
+++ confs/home/geoff/.xmonad/lib/ClickableHook.hs
@@ -2,9 +2,9 @@
 -- |
 -- Module : ClickableHook
 --
+-- NOTE: requires `xdotool`
 -----------------------------------------------------------------------------
-module ClickableHook ( clickableNames
-                     , getSortByClickableIndex
+module ClickableHook ( getSortByClickableIndex
                      , doClickableHook
                      , undoClickableHook
                      ) where
@@ -16,29 +16,26 @@ import XMonad.Util.WorkspaceCompare
 import Data.List
 import Data.Function (on)
 
+-- In case workspace tags include any '<', escape them
 xmobarEscape :: String -> String
 xmobarEscape = concatMap doubleLts
   where
         doubleLts '<' = "<<"
         doubleLts x   = [x]
 
-confWorkspaces :: X ([String])
-confWorkspaces = asks (workspaces . config) 
+-- Get workspace tags from user config (e.g. myWorkspaces)
+configWorkspaces :: X [String]
+configWorkspaces = asks (workspaces . config) 
 
--- confWorkspaces' :: [String]
--- confWorkspaces' = concat $ asks (workspaces . config) 
-
--- clickableNames :: [String]
--- clickableNames = 
---   [ "<action=xdotool key super+" ++ show i ++ ">" ++ (xmobarEscape ws) ++ "</action>"
---     | (i, ws) <- zip [1 :: Integer .. 9] confWorkspaces ]
-
-clickableNames :: X ([String])
-clickableNames = do
-  spaces <- confWorkspaces
+-- Wrap workspace tags with on-click switch action
+clickableWorkspaces :: X [String]
+clickableWorkspaces = do
+  spaces <- configWorkspaces
   return
-    [ "<action=xdotool key super+" ++ show i ++ ">" ++ (xmobarEscape ws) ++ "</action>"
-      | (i, ws) <- zip [1 :: Integer .. 9] spaces ]
+    [ "<action=xdotool key super+" ++ show i ++ ">"
+      ++ (xmobarEscape ws)
+      ++ "</action>"
+    | (i, ws) <- zip [1 :: Integer .. 9] spaces ]
 
 -- | From XMonad.Util.WorkspaceCompare (not exported)
 -- Compare Maybe's differently, so Nothing (i.e. workspaces without indexes)
@@ -49,49 +46,34 @@ indexCompare Nothing (Just _) = GT
 indexCompare (Just _) Nothing = LT
 indexCompare a b = compare a b
 
--- getClickableIndex :: X (WorkspaceId -> Maybe Int)
--- getClickableIndex = return $ flip elemIndex clickableNames
 getClickableIndex :: X (WorkspaceId -> Maybe Int)
 getClickableIndex = do
-  names <- clickableNames
-  return $ flip elemIndex names
+  clicks <- clickableWorkspaces
+  return $ flip elemIndex clicks
 
 getClickableCompare :: X WorkspaceCompare
 getClickableCompare = do
   clickableSort <- getClickableIndex
   return $ mconcat [indexCompare `on` clickableSort, compare]
 
+-- Sort workspaces by index of clickableWorkpaces (for ppSort)
 getSortByClickableIndex :: X WorkspaceSort
 getSortByClickableIndex = mkWsSort getClickableCompare
 
--- doClickableHook :: X ()
--- doClickableHook = do
---   _ <- sequence
---        $ map (\(w, c) -> modifyWindowSet $ \s -> W.renameTag w c s)
---              (zip confWorkspaces clickableNames)
---   return ()
-
--- undoClickableHook :: X ()
--- undoClickableHook = do
---   _ <- sequence
---        $ map (\(w, c) -> modifyWindowSet $ \s -> W.renameTag c w s)
---              (zip confWorkspaces clickableNames)
---   return ()
-
 doClickableHook :: X ()
 doClickableHook = do
-  spaces <- confWorkspaces
-  names <- clickableNames
+  spaces <- configWorkspaces
+  clicks <- clickableWorkspaces
   _ <- sequence
        $ map (\(w, c) -> modifyWindowSet $ \s -> W.renameTag w c s)
-             (zip spaces names)
+             (zip spaces clicks)
   return ()
 
 undoClickableHook :: X ()
 undoClickableHook = do
-  spaces <- confWorkspaces
-  names <- clickableNames
+  spaces <- configWorkspaces
+  clicks <- clickableWorkspaces
   _ <- sequence
        $ map (\(w, c) -> modifyWindowSet $ \s -> W.renameTag c w s)
-             (zip spaces names)
+             (zip spaces clicks)
   return ()
