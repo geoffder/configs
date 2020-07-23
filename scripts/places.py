653643a37f2008f47f1d86687690597b5d1010ad
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-07-23 11:53:33 -0700

    Organize config places list.

--- scripts/places.py
+++ scripts/places.py
@@ -3,39 +3,47 @@ import os
 home = os.path.expanduser("~/")
 base = home + "GitRepos/configs/confs/"
 
-# full path for each config to copy
+# full path (or directory) for each config to copy
 files = [
+    # system themeing
     home + ".Xresources",
-    home + ".zshrc",
     home + ".gtkrc-2.0",
+    home + ".config/gtk-3.0/settings.ini",
+    home + ".config/qt5ct/qt5ct.conf",
+    home + ".themes/Adapta-DeepPurple-Nokto-Eta",
+    home + ".themes/Papirus-Dark-Places-Violet",
+    home + ".themes/Papirus-Adapta-Nokto-Violet",
+    home + ".config/picom.conf",
+    # shell / terminal
+    home + ".zshrc",
     home + ".oh-my-zsh/custom/themes",
-    home + ".i3/config",
+    home + ".fsh/monokai.ini",
+    home + ".config/kitty",
+    home + "miniconda3/etc/conda/activate.d",
+    home + "miniconda3/envs/neuron/etc/conda/activate.d",
+    # editors
+    home + ".doom.d",
+    home + ".config/nvim/init.vim",
+    # applications
+    home + ".config/mimeapps.list",
+    home + ".config/ranger/rc.conf",
+    home + ".config/ranger/rifle.conf",
+    home + ".config/rofi",
+    home + ".config/flameshot/flameshot.conf",
+    home + ".config/firefox",
+    # XMonad
     home + ".xmonad/xmonad.hs",
     home + ".xmonad/xmonad_notes.md",
     home + ".xmonad/xpm",
     home + ".xmonad/lib/ClickableWsHook.hs",
     home + ".xmonad/lib/ClickableWorkspaces.hs",
-    home + ".doom.d",
+    home + ".config/xmobar",
+    # Openbox
     home + ".themes/Monokai-ish",
-    home + ".themes/Adapta-DeepPurple-Nokto-Eta",
-    home + ".themes/Papirus-Dark-Places-Violet",
-    home + ".themes/Papirus-Adapta-Nokto-Violet",
-    home + ".config/i3-scrot.conf",
-    home + ".config/i3status/config",
     home + ".config/openbox",
     home + ".config/polybar",
-    home + ".config/xmobar",
-    home + ".config/mimeapps.list",
-    home + ".config/picom.conf",
-    home + ".config/kitty",
-    home + ".config/nvim/init.vim",
-    home + ".config/ranger/rc.conf",
-    home + ".config/ranger/rifle.conf",
-    home + ".config/rofi",
-    home + ".config/flameshot/flameshot.conf",
-    home + ".config/gtk-3.0/settings.ini",
-    home + ".config/firefox",
-    home + ".config/qt5ct/qt5ct.conf",
-    home + "miniconda3/etc/conda/activate.d",
-    home + "miniconda3/envs/neuron/etc/conda/activate.d",
+    # i3
+    home + ".i3/config",
+    home + ".config/i3-scrot.conf",
+    home + ".config/i3status/config",
 ]
