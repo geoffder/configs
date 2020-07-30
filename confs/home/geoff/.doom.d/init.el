9e513a7d3ed2d3be8775979bfc0b093d716397f6
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-07-29 23:41:09 -0700

    Re-enable fsharp, disable treemacs + unicode.

--- confs/home/geoff/.doom.d/init.el
+++ confs/home/geoff/.doom.d/init.el
@@ -37,8 +37,8 @@
         +defaults)         ; default popup rules
        ;pretty-code        ; replace bits of code with pretty symbols
        tabs                ; an tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ; treemacs          ; a project drawer, like neotree but cooler
+       ; unicode           ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        window-select       ; visually switch windows
@@ -116,9 +116,9 @@
        ;;erlang            ; an elegant language for a more civilized age
        ess                 ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
-       ;; (fsharp
-       ;;  +lsp
-       ;;  )                  ; ML stands for Microsoft's Language
+       (fsharp
+        +lsp
+        )                  ; ML stands for Microsoft's Language
        ;;Go                ; The hipster dialect
        (haskell +dante)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
