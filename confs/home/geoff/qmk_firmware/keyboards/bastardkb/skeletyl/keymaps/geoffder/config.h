1f5a08fe73c9ca23900f793250248489f70def9d
Geoff deRosenroll <geoffderosenroll@gmail.com>
2021-05-19 23:12:48 -0700

    Mouse key tweaks (WIP) and default unicode output

--- confs/home/geoff/qmk_firmware/keyboards/bastardkb/skeletyl/keymaps/geoffder/config.h
+++ confs/home/geoff/qmk_firmware/keyboards/bastardkb/skeletyl/keymaps/geoffder/config.h
@@ -7,7 +7,7 @@
 #define MASTER_LEFT
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 175
+#define TAPPING_TERM 150
 
 // Use get_tappihg_term in keymap.c to adjust terms for specific keys
 #define TAPPING_TERM_PER_KEY
@@ -22,3 +22,12 @@
 // NOTE: It seems like I roll over between keys a lot, so this leads to a lot
 // of unintenional mod presses for me on the home row.
 // #define PERMISSIVE_HOLD
+
+// mouse key configuration
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
+#define UNICODE_SELECTED_MODES UC_LNX
